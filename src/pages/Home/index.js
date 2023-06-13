d06d9dfcccba7d8074c49c59cb3ba438a2762efc
Wajid Ali Shah <wajidafridi48@gmail.com>
2023-06-13 13:27:43 +0500

    landing page css updated

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,11 +4,25 @@ import styles from "./index.module.scss";
 
 const HomeScreen = () => {
   return (
-    <div className={styles.homePage}>
-      <h1 className={styles.title}>Welcome to the Personality Test!</h1>
-      <Link to="/test" className="button">
-        Start Test
-      </Link>
+    <div className={`${styles.homePage}`}>
+      <div className="section flex">
+        <div className={`flex column ${styles.left}`}>
+          <h1 className={styles.title}>Personality Test Project</h1>
+          <p className={styles.description}>
+            Take the test.
+            <br />
+            Explore your personality.
+            <br />
+            Advance the study of individual differences.
+            <br />
+          </p>
+        </div>
+        <div className={`${styles.right}`}>
+          <Link to="/test" className={`button ${styles.button}`}>
+            Start The Test
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
