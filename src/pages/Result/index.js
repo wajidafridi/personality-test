771878bed92763ca69dc14fbf02844f307736f52
Wajid Ali Shah <wajidafridi48@gmail.com>
2023-06-13 16:59:06 +0500

    result page css updated

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,7 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 // @import context
 import { AppContext } from "../../context";
+// @import styles
+import styles from "./index.module.scss";
 
 const ResultScreen = () => {
   const navigate = useNavigate();
@@ -29,9 +32,12 @@ const ResultScreen = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Your Personality Trait</h2>
-      <p>{personalityTrait}</p>
+    <div className={styles.resultPage}>
+      <div className={`${styles.resultContainer} section`}>
+        <h2>Your Personality Trait</h2>
+        <p className={styles.type}>{personalityTrait}</p>
+        <Link to="/">Go to Home Page</Link>
+      </div>
     </div>
   );
 };
