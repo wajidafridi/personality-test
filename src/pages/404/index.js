08a7446998036bd0bd3cf79ec350cc0113f72304
Wajid Ali Shah <wajidafridi48@gmail.com>
2023-06-13 16:59:27 +0500

    404 page css updated

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// @import styles
+import styles from "./index.module.scss";
 
 const NotFound = () => {
   return (
-    <div className="page">
-      <h2>Page Not Found</h2>
-      <p>The requested page does not exist.</p>
-      <Link to="/" className="home-link">
-        Go to Home Page
-      </Link>
+    <div className={styles.errorPage}>
+      <div className={`section ${styles.errorContainer}`}>
+        <h2>Page Not Found</h2>
+        <p className={styles.description}>The requested page does not exist.</p>
+        <Link to="/" className="home-link">
+          Go to Home Page
+        </Link>
+      </div>
     </div>
   );
 };
