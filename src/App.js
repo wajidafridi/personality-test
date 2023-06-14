5bba6e5180bddb6da73c82535542b592419da754
Wajid Ali Shah <wajidafridi48@gmail.com>
2023-06-14 17:44:48 +0500

    site logo added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Link, Route, Routes, BrowserRouter } from "react-router-dom";
 
 import { AppContextProvider } from "./context";
+// @import pages
+import NotFound from "./pages/404";
 import HomeScreen from "./pages/Home";
 import TestScreen from "./pages/Test";
 import ResultScreen from "./pages/Result";
-import NotFound from "./pages/404";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeScreen />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/test",
-    element: <TestScreen />,
-  },
-  {
-    path: "/result",
-    element: <ResultScreen />,
-  },
-]);
+import Logo from "./media/logo.png";
 
 const App = () => {
   return (
-    <div className="App">
-      <AppContextProvider>
-        <RouterProvider router={router} />
-      </AppContextProvider>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <AppContextProvider>
+          <header className="header">
+            <Link to="/">
+              <img
+                src={Logo}
+                title="Home"
+                alt="site-logo"
+                className="siteLogo"
+              />
+            </Link>
+          </header>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/test" element={<TestScreen />} />
+            <Route path="/result" element={<ResultScreen />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AppContextProvider>
+      </div>
+    </BrowserRouter>
   );
 };
 
